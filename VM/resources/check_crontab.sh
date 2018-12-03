c469a7118c6b76fae0034165d42f65806a8897bf
Emmanuel Ruaud <eruaud@student.le-101.fr>
2018-12-03 11:32:25 -0500

    check_crontab in log

--- VM/resources/check_crontab.sh
+++ VM/resources/check_crontab.sh
@@ -1,7 +1,7 @@
-if md5sum -c cron_fingerprint; then
+if md5sum -c /var/log/cron_fingerprint.log; then
 	echo "/etc/crontab is unchanged"
 else
 	echo "/etc/crontab was modified on your server" | mail -s "/etc/crontab was modified on your server" root
 fi
-md5sum /etc/crontab > cron_fingerprint
+md5sum /etc/crontab > /var/log/cron_fingerprint.log
 
