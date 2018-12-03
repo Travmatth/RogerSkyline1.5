64b54f4691652d78fe6f700de7b64cfec6d4182e
Emmanuel Ruaud <eruaud@student.le-101.fr>
2018-12-03 11:15:22 -0500

    emails

--- VM/admin_config.sh
+++ VM/admin_config.sh
@@ -1,7 +1,12 @@
-apt install vim sudo
+apt-get install -y vim sudo mailutils
 usermod -aG sudo oreo
 cp resources/interfaces /etc/network/interfaces
 echo "Port 21" >> /etc/ssh/sshd_config
 service ssh restart
 service networking restart
 iptables-restore < resources/iptables
+mkdir /root/scripts
+cp resources/update_script.sh /root/scripts/
+cp resources/check_crontab.sh /root/scripts/
+crontab resources/crontab.bak
+cp resources/aliases /etc/aliases
