4f0035cf418483f2659d67a439dc3328dbc99232
Emmanuel Ruaud <eruaud@student.le-101.fr>
2018-12-03 13:27:34 -0500

    selfsigned

--- VM/admin_config.sh
+++ VM/admin_config.sh
@@ -11,4 +11,6 @@ cp resources/update_script.sh /root/scripts/
 cp resources/check_crontab.sh /root/scripts/
 crontab resources/crontab.bak
 cp resources/aliases /etc/aliases
-cp resources/html/* /var/www/html 
+cp resources/html/* /var/www/html
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+cp resources/self-signed.conf /etc/nginx/snippets/self-signed.conf
