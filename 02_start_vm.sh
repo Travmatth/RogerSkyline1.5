b594b676b935f00a0ac6a542f787aa1d79c6f76c
Emmanuel Ruaud <ruaudemmanuel@hotmail.fr>
2018-12-04 14:40:07 +0100

    update to vogsphere

--- 02_start_vm.sh
+++ 02_start_vm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# A script to move a directory to the VM and launch ssh
-VBoxHeadless -s AutoVM -v on
+VBoxHeadless -s AutoVM -v on &
---
+++ 05_stop_vm.sh
@@ -0,0 +1,1 @@
+VBoxManage controlvm AutoVM poweroff soft
